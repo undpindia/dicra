c3972b1f299b0c393978ffb6930b70fe0a3a70a7
Ambarish Narayanan <32821704+ambarish-narayanan@users.noreply.github.com>
2022-03-07 01:09:21 +0530

    Update job.py

--- DiCRA Plaform/src/01_API/models/job.py
+++ DiCRA Plaform/src/01_API/models/job.py
@@ -2,11 +2,9 @@ from sqlalchemy import Integer, String
 from sqlalchemy.sql.schema import Column
 from db.database import Base
 
-
 class Job(Base):
     __tablename__ = 'jobs'
 
     id = Column(Integer, primary_key=True)
     title = Column(String, nullable=False)
     description = Column(String, nullable=False)
-

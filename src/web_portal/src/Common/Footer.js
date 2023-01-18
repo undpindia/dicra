064be2d7a676c974bbb15abe765f5711e0a33b83
Maneesh7012 <maneesh@misteo.co>
2023-01-18 23:15:19 +0530

    link removed

--- src/web_portal/src/Common/Footer.js
+++ src/web_portal/src/Common/Footer.js
@@ -8,9 +8,6 @@ export default class Footer extends Component {
           &nbsp;
           <span  style={{marginRight:"10px"}}><Link to="/policy" style={{color:"#90989B", fontSize:"13px"}}>Privacy policy</Link></span>
           &nbsp;
-          <span  style={{marginRight:"10px"}}><Link to="/field-stories" style={{color:"#90989B", fontSize:"13px"}}>Field Stories</Link></span>
-          &nbsp;
-          <span  style={{}}><Link to="/data4policy" style={{color:"#90989B", fontSize:"13px"}}>Data4Policy</Link></span>
         </p>
     );
   }

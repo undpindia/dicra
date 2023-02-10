873148f97ca172d7da0e5c60f64cf8024b4fbe2e
Maneesh7012 <maneesh@misteo.co>
2023-02-10 10:07:37 +0530

    added pdf

--- src/web_portal/src/pages/FieldStories.js
+++ src/web_portal/src/pages/FieldStories.js
@@ -1,18 +1,20 @@
-import React from "react";
+import React, { useState } from 'react';
 import "../Common/common.css";
 import Header from "../Common/Header";
 import { BiX } from "react-icons/bi";
 import { Link } from "react-router-dom";
 import { Row, Col} from "reactstrap";
+import { Document, Page, pdfjs  } from 'react-pdf';
+import PDF from "../pdf/Climate Resilient Agriculture_Filed Stories_Telangana.pdf"
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
-import { connect } from "react-redux";
-const mapStateToProps = (ReduxProps) => {
-  return {
-    Layers: ReduxProps.Layers,
-  };
-};
-class Terms extends React.Component {
-  render() {
+const FieldStories = () => {
+    const [numPages, setNumPages] = useState(null);
+    const [pageNumber] = useState(1);
+  
+    function onDocumentLoadSuccess({ numPages }) {
+      setNumPages(numPages);
+    }
     return (
       <React.Fragment>
         <div className="page-header">
@@ -30,10 +32,16 @@ class Terms extends React.Component {
             <div>
               <div className="container about-page">
                 <Row>
-                  <Col className="about-content">
-                    <p style={{ fontSize: "16px" }}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Convallis convallis tellus id interdum. Auctor eu augue ut lectus arcu bibendum at. Tristique risus nec feugiat in fermentum posuere. Scelerisque eu ultrices vitae auctor eu augue ut lectus arcu. Condimentum lacinia quis vel eros. In tellus integer feugiat scelerisque. Fames ac turpis egestas sed tempus urna et pharetra pharetra. Sit amet justo donec enim diam vulputate ut pharetra. Aliquam sem et tortor consequat id porta nibh. Commodo elit at imperdiet dui accumsan sit amet. Urna neque viverra justo nec ultrices dui. Eu non diam phasellus vestibulum lorem sed. Quis ipsum suspendisse ultrices gravida dictum. Massa vitae tortor condimentum lacinia quis vel eros donec ac. Ultricies mi quis hendrerit dolor.
-                    </p>
+                  <Col className="pdf-wrapper">
+                    <Document
+                    file={PDF}
+                    onLoadSuccess={onDocumentLoadSuccess}
+                >{Array.apply(null, Array(numPages))
+                  .map((x, i)=>i+1)
+                  .map(page => <Page pageNumber={page}/>)}
+                    
+                </Document>
+      <p>Page {pageNumber} of {numPages}</p>
                   </Col>
                 </Row>
               </div>
@@ -42,6 +50,5 @@ class Terms extends React.Component {
         </div>
       </React.Fragment>
     );
-  }
-}
-export default connect(mapStateToProps)(Terms);
+};
+export default FieldStories;

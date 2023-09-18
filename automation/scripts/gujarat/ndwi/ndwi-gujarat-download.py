29efe238df0a66c25d5fb6ac6233139a1fde85b5
Maneesh7012 <maneesh@misteo.co>
2023-09-18 17:56:29 +0530

    lst,ndwi download script

--- automation/scripts/gujarat/ndwi/ndwi-gujarat-download.py
+++ automation/scripts/gujarat/ndwi/ndwi-gujarat-download.py
@@ -7,7 +7,7 @@ from datetime import date, datetime, timedelta
 #Every file type and state base folder should be set in the beginning of the program
 statebase = '/nfsdata/ndwi/gujarat'
 scriptbase= statebase + '/download'
-basepath= statebase + '/base'
+basepath=  '/nfsdata/base_shapefiles'
 tifspath = scriptbase + '/GEE_NDWItifs'
 
 today = date.today() 
@@ -28,7 +28,7 @@ ee.Initialize() # Initialize
 #Append state's boundary data address
 #f=  open('../../../../../src/data_preprocessing/base_geojson/TL_state_shapefile_for_clip.geojson')
 #Enter path of the config files, always prefix the scriptbase path to avoid referring wrong folders
-f=  open(basepath+'/Gujarat_state_shapefile_for_clip.geojson')
+f=  open(basepath+'/gj_state_boundary.geojson')
 data =json.load(f)
 
 data = data['features'][0]['geometry']['coordinates'][0]
@@ -41,16 +41,16 @@ image = collection_subset.first().select('NDWI')  # Pick the first image from th
 geemap.image_props(image).getInfo() # Finds basic info of this image
 
 
-if tifspath not in os.listdir():
-    os.mkdir(tifspath)
-else:
-    print('GEE_NDWItifs directory exists')
+#if tifspath not in os.listdir():
+#    os.mkdir(tifspath)
+#else:
+#    print('GEE_NDWItifs directory exists')
 
-#out = os.path.join('GEE_NDVItifs') # Set path to where we want to save the GeoTIF
+#out = os.path.join('GEE_NDWItifs') # Set path to where we want to save the GeoTIF
 out = os.path.join(tifspath) # Set path to where we want to save the GeoTIF
 # Now export each image within the collection to a GeoTIF
 geemap.ee_export_image_collection(collection_subset, out_dir = out, scale=image.select('NDWI').projection().nominalScale(), region=roi, file_per_band=True, crs='EPSG:4326')
 
-# image.select('NDVI').projection().nominalScale() sets the scale equal to the resolution of the images within the collection
+# image.select('NDWI').projection().nominalScale() sets the scale equal to the resolution of the images within the collection
 # file_per_band=False: all bands are downloaded and put as one file
 # file_per_band=True: each band is downloaded in a single image
